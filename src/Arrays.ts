b32d5ee1f499d87360293b272e225bcbf1045f71
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2024-12-09 17:00:40 -0800

    rotate array problem solved + file update

--- src/Arrays.ts
+++ src/Arrays.ts
@@ -1,5 +1,5 @@
 /**
- * 88. Merge Sorted Array
+ * 88. Merge Sorted Array - Easy
  *
  * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n,
  * representing the number of elements in nums1 and nums2 respectively.
@@ -9,49 +9,57 @@
  * The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
  * To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should
  * be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ *
+ * Time Complexity: O(N)
+ *
+ * Space Complexity: O(1)
  */
-// function merge(
-//   nums1: number[],
-//   m: number,
-//   nums2: number[],
-//   n: number
-// ): number[] | null {
-//   // pointer for last valid index of nums1 //
-//   let p1 = m - 1;
-//   // pointer for last index of nums2 //
-//   let p2 = n - 1;
-//   // pointer for last index of nums1 //
-//   let p = m + n - 1;
+/*
+function merge(
+  nums1: number[],
+  m: number,
+  nums2: number[],
+  n: number
+): number[] | null {
+  // pointer for last valid index of nums1 //
+  let p1 = m - 1;
+  // pointer for last index of nums2 //
+  let p2 = n - 1;
+  // pointer for last index of nums1 //
+  let p = m + n - 1;
 
-//   // start merging from the end of nums1 //
-//   while (p1 >= 0 && p2 >= 0) {
-//     if (nums1[p1] > nums2[p2]) {
-//       nums1[p] = nums1[p1];
-//       p1--;
-//     } else {
-//       nums1[p] = nums2[p2];
-//       p2--;
-//     }
-//     p--;
-//   }
+  // start merging from the end of nums1 //
+  while (p1 >= 0 && p2 >= 0) {
+    if (nums1[p1] > nums2[p2]) {
+      nums1[p] = nums1[p1];
+      p1--;
+    } else {
+      nums1[p] = nums2[p2];
+      p2--;
+    }
+    p--;
+  }
 
-//   // copy any remaining elements from nums2 //
-//   // num1's elements are already in place //
-//   while (p2 >= 0) {
-//     nums1[p] = nums2[p2];
-//     p2--;
-//     p--;
-//   }
+  // copy any remaining elements from nums2 //
+  // num1's elements are already in place //
+  while (p2 >= 0) {
+    nums1[p] = nums2[p2];
+    p2--;
+    p--;
+  }
 
-//   return nums1;
-// }
+  return nums1;
+}
 
-// console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
-// console.log(merge([1], 1, [], 0));
-// console.log(merge([0], 0, [1], 1));
+// testing //
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)); // [1, 2, 2, 3, 5, 6]
+console.log(merge([1], 1, [], 0)); // [1]
+console.log(merge([0], 0, [1], 1)); // [1]
+*/
 
 /**
- * 26. Remove Duplicates From Sorted Array
+ * 26. Remove Duplicates From Sorted Array - Easy
+ *
  * Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each
  * unique element appears only once. The relative order of the elements should be kept the same. Then return
  * the number of unique elements in nums.
@@ -60,28 +68,35 @@
  * Change the array nums such that the first k elements of nums contain the unique elements in the order they
  * were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
  * Return k.
+ *
+ * Time Complexity: O(N)
+ *
+ * Space Complexity: O(1)
  */
-// function removeDuplicates(nums: number[]): number {
-//   if (nums.length === 0) return 0;
+/*
+function removeDuplicates(nums: number[]): number {
+  if (nums.length === 0) return 0;
 
-//   let uniqueIndex = 0;
+  let uniqueIndex = 0;
 
-//   for (let i = 1; i < nums.length; i++) {
-//     if (nums[uniqueIndex] !== nums[i]) {
-//       uniqueIndex++; // move the unique index forward
-//       nums[uniqueIndex] = nums[i]; // place the unique element in the correct position
-//     }
-//   }
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[uniqueIndex] !== nums[i]) {
+      uniqueIndex++; // move the unique index forward
+      nums[uniqueIndex] = nums[i]; // place the unique element in the correct position
+    }
+  }
 
-//   return uniqueIndex + 1; // count of unique elements
-// }
+  return uniqueIndex + 1; // count of unique elements
+}
 
-// console.log(removeDuplicates([1, 1, 2, 2, 3, 3]));
-// console.log(removeDuplicates([]));
-// console.log(removeDuplicates([1, 2, 2, 3, 3, 3, 4, 4]));
+// testing //
+console.log(removeDuplicates([1, 1, 2, 2, 3, 3])); // 3
+console.log(removeDuplicates([])); // 0
+console.log(removeDuplicates([1, 2, 2, 3, 3, 3, 4, 4])); // 4
+*/
 
 /**
- * 169. Majority Element
+ * 169. Majority Element - Easy
  *
  * Given an array nums of size n, return the majority element.
  * The majority element is the element that appears more than (n / 2) times.
@@ -119,11 +134,6 @@ function majorityElement1(nums: number[]): number {
 
   throw new Error("No majority element found.");
 }
-
-// testing //
-console.log(majorityElement1([3, 2, 3])); // Expected: 3
-console.log(majorityElement1([2, 2, 1, 1, 1, 2, 2])); // Expected: 2
-console.log(majorityElement1([1, 2, 1, 2])); // Expected: Error
 */
 
 /**
@@ -135,6 +145,7 @@ console.log(majorityElement1([1, 2, 1, 2])); // Expected: Error
  * Space Complexity: O(1) - We use a fixed number of variables (candidate and count) regardless of input size,
  * no hash maps or arrays.
  */
+/*
 function majorityElement2(nums: number[]): number {
   let candidate = 0;
   let count = 0;
@@ -165,6 +176,63 @@ function majorityElement2(nums: number[]): number {
 }
 
 // testing //
-console.log(majorityElement2([3, 2, 3])); // Expected: 3
-console.log(majorityElement2([2, 2, 1, 1, 1, 2, 2])); // Expected: 2
-console.log(majorityElement2([2, 1, 1, 2])); // Expected: Error
+console.log(majorityElement1([3, 2, 3])); // 3
+console.log(majorityElement1([2, 2, 1, 1, 1, 2, 2])); // 2
+console.log(majorityElement1([1, 2, 1, 2])); // Error
+
+console.log(majorityElement2([3, 2, 3])); // 3
+console.log(majorityElement2([2, 2, 1, 1, 1, 2, 2])); // 2
+console.log(majorityElement2([2, 1, 1, 2])); // Error
+*/
+
+/**
+ * 189. Rotate Array - Medium
+ *
+ * Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+ */
+
+/**
+ * Utilizing pop() and unshift().
+ *
+ * Time Complexity: O(K * N).
+ *
+ * Space Complexity: O(1).
+ */
+function rotateArray1(nums: number[], k: number): number[] {
+  for (let i = 0; i < k; i++) {
+    let poppedNumber = nums.pop()!; // O(1) //
+    nums.unshift(poppedNumber); // O(N) //
+  }
+
+  return nums;
+}
+
+/**
+ * Utilizing more memory.
+ *
+ * Time Complexity: O(N) - loop iterated over all N elements once.
+ *
+ * Space Complexity: O(N) - created a new array to store rotated elements.
+ */
+function rotateArray2(nums: number[], k: number): number[] {
+  const rotatedNums: number[] = [];
+
+  // handle cases where k > n //
+  k = k % nums.length;
+
+  // fill new array with rotated elements //
+  for (let i = 0; i < nums.length; i++) {
+    rotatedNums[(i + k) % nums.length] = nums[i];
+  }
+
+  return rotatedNums;
+}
+
+// testing //
+console.log("rotate array 1:");
+console.log(rotateArray1([1, 2, 3, 4, 5, 6, 7], 3)); // [5, 6, 7, 1, 2, 3, 4]
+console.log(rotateArray1([-1, -100, 3, 99], 2), "\n"); // [3, 99, -1, -100]
+
+console.log("rotate array 2:");
+console.log(rotateArray2([1, 2, 3, 4, 5, 6, 7], 3));
+console.log(rotateArray2([-1, -100, 3, 99], 2));
